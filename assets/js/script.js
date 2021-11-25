1fe0e86a8a7020d61a3b472c3cf6a92294e66520
MarceloNegromonte <mjncavalcante@gmail.com>
2021-11-25 17:08:10 -0300

    implementação js

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+/*
+Case sensitive = reconhece letras maiuscula e minuscula
+
+acesso por Tag: getElementeByTagName()
+por Id: getElementById()
+por Nome: getElementsByName()
+por Classe: getElementsByClassName()
+por Seletor: querySelector()
+*/
+
+let nome = document.getElementById('nome')
+let email = document.querySelector('#email')
+let assunto = document.querySelector('#assunto')
+
+nome.style.width = '100%'
+email.style.width = '100%'
+
+function validaNome() {
+    let txtNome = document.querySelector('#txtNome')
+    if(nome.value.length < 3){
+        txtNome.innerHTML = 'Nome Inválido'
+        txtNome.style.color = 'red'
+    } else {
+        txtNome.innerHTML = 'Nome Válido'
+        txtNome.style.color = 'green'
+    }
+}
+
+function validaEmail() {
+    let txtEmail = document.querySelector('#txtEmail')
+    if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1) {
+        txtEmail.innerHTML = 'E-mail Inválido'
+        txtEmail.style.color = 'red'
+    } else {
+        txtEmail.innerHTML = 'E-mail Válido'
+        txtEmail.style.color = 'green'
+    }
+}
